b8f28f5c8d656896cdd8cd2bf9f120e96a763992
Charles Pereira <charlesti.ptu@gmail.com>
2020-10-04 19:34:04 -0300

    configurando context useHook api com ts

---
+++ src/components/Model/useModel.ts
@@ -0,0 +1,22 @@
+import { useContext, useEffect } from 'react';
+
+import ModelsContext from './ModelsContext';
+
+export default function useModel(modelName: string) {
+  const { registerModel, unregisterModel, getModelByName } = useContext(
+    ModelsContext,
+  );
+
+  useEffect(
+    () => () => unregisterModel(modelName),
+
+    [modelName, unregisterModel],
+  );
+
+  const getModel = useContext(() => getModelByName(modelName), [
+    getModelByName,
+    modelName,
+  ]);
+
+  return { registerModel, getModel };
+}
