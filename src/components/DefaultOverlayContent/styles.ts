2409736f38bb159c605aad46401b895c20510ad0
Charles Pereira <charlesti.ptu@gmail.com>
2020-10-06 23:13:26 -0300

    defaultOerlay

--- src/components/DefaultOverlayContent/styles.ts
+++ src/components/DefaultOverlayContent/styles.ts
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 1;
+`;
 
-export const Heading = styled.div``;
+export const Heading = styled.div`
+  margin-top: 16.5vh;
 
-export const Buttons = styled.div``;
+  > h1 {
+    font-weight: 400;
+  }
+`;
+
+export const Buttons = styled.div`
+  width: 100%;
+  text-align: center;
+`;
