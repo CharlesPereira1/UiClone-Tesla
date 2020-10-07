a5314b2c2402d8dc99cbb6442804937a971c8793
Charles Pereira <charlesti.ptu@gmail.com>
2020-10-07 00:28:04 -0300

    useWhapperScroll

--- src/components/Model/useWhapperScroll.ts
+++ src/components/Model/useWhapperScroll.ts
@@ -1,8 +1,23 @@
 import { useMotionValue } from 'framer-motion';
+import { useContext, useEffect } from 'react';
+import ModelsContext from './ModelsContext';
 
 export default function useWrapperScroll() {
+  const { wrapperRef } = useContext(ModelsContext);
+
   const scrollY = useMotionValue(0);
   const scrollYProgress = useMotionValue(0);
+
+  useEffect(() => {
+    if (wrapperRef.current) {
+      const updateScrollValue = () => {};
+
+      wrapperRef.current.addEventListener('scroll', updateScrollValue);
+
+      return () =>
+        wrapperRef.current?.removeEventListener('scroll', updateScrollValue);
+    }
+  }, [wrapperRef]);
 
   return { scrollY, scrollYProgress };
 }
